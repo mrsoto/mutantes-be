3d8fd28f441414d40b594a1155be41cd91ef0f7c
mrsoto <mrsoto@users.noreply.github.com>
2020-02-06 22:35:46 -0300

    Versión inicial

---
+++ src/main/java/me/mrs/mutantes/servicios/MutantesServiciosApplication.java
@@ -0,0 +1,13 @@
+package me.mrs.mutantes.servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantesServiciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutantesServiciosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mrs/mutantes/servicios/MutantesServiciosApplicationTests.java
@@ -0,0 +1,14 @@
+package me.mrs.mutantes.servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantesServiciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // start
+    }
+
+}
