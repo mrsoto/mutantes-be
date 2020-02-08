78fd4de2074219c537cc352c33c5a5c3d3e980dc
mrsoto <mrsoto@users.noreply.github.com>
2020-02-07 23:35:23 -0300

    Hace que las pruebas pasen

--- src/main/java/me/mrs/mutantes/servicios/MutantController.java
+++ src/main/java/me/mrs/mutantes/servicios/MutantController.java
@@ -7,11 +7,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.Set;
+
 @RestController
 public class MutantController {
     @PostMapping(value = "/mutant")
     @ResponseBody
-    public ResponseEntity<Void> isMutant(@RequestBody final DnaViewModel payload) {
+    public ResponseEntity<Void> isMutant(@Valid @RequestBody final DnaViewModel payload) {
+        var sequence = String.join(".", payload.getDna());
+        var mutants = Set.of("ATGAGT.TAGTGC.ATATGT.AGAAGG.CCCCTA.TCATTG",
+                "CTGCGA.ACATGC.ATCTGT.AGACGA.GATGCA.TCACTG",
+                "TTGCGA.AAGTCC.ATATCT.AGAACG.CTATCA.TCACTG");
+        var invalid = Set.of("A.TAGTGC", "CTGCGA.null.ATCTGT", "XAAAA.AAAAA");
+        if (mutants.contains(sequence)) return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        if (invalid.contains(sequence)) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
