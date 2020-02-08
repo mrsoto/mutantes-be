b41189c46767c44cba5b30736c548c7e496da7cf
mrsoto <mrsoto@users.noreply.github.com>
2020-02-08 12:51:39 -0300

    Refactoriza el validator  Make it better

--- src/main/java/me/mrs/mutantes/servicios/DnaValidator.java
+++ src/main/java/me/mrs/mutantes/servicios/DnaValidator.java
@@ -3,13 +3,19 @@ package me.mrs.mutantes.servicios;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Collection;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DnaValidator implements ConstraintValidator<DnaConstraint, Collection<String>> {
+
+    public static final String VALID_SYMBOLS = "^[ATGC]+$";
+
     @Override
-    public boolean isValid(Collection<String> dna, ConstraintValidatorContext constraintValidatorContext) {
-        // TODO: Implement validator
-        var invalid = Set.of("", "A.TAGTGC", "CTGCGA.null.ATCTGT", "XAAAA.AAAAA");
-        return !invalid.contains(String.join(".", dna));
+    public boolean isValid(Collection<String> dna,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        if (dna == null || dna.isEmpty() || dna.contains(null)) return false;
+
+        if (dna.stream().map(String::length).collect(Collectors.toSet()).size() != 1) return false;
+
+        return dna.stream().allMatch(sequence -> sequence.matches(VALID_SYMBOLS));
     }
 }
