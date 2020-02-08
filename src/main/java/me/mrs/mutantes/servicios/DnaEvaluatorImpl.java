1bd235c6f92fa57f0cc6ba1f45824580909ce6c2
mrsoto <mrsoto@users.noreply.github.com>
2020-02-08 19:19:35 -0300

    fix isMutant signature

--- src/main/java/me/mrs/mutantes/servicios/DnaEvaluatorImpl.java
+++ src/main/java/me/mrs/mutantes/servicios/DnaEvaluatorImpl.java
@@ -14,7 +14,7 @@ public class DnaEvaluatorImpl implements DnaEvaluator {
      * Evaluate a DNA looking for mutations. A mutation is identified by for linearly continuous
      * symbols in any direction.
      * <p>
-     * Preconditions: DNA should be valid, See {@link DnaValidator}
+     *
      * <p>Algorithm <b>O(S x B)</b> or <b>O(N<sup>2</sup>)</b> or less for an ADN of
      * {@code S} sequences and  {@code B} bases per sequence.
      * <p>
@@ -22,12 +22,12 @@ public class DnaEvaluatorImpl implements DnaEvaluator {
      * E).</p>
      * <p>
      *
-     * <p><em>Notas:</em>
+     * <p><em>Notes:</em>
      * <ul><li>
-     *     Consumo de memoria <b>2 * O(NxM)</b>
+     *     Memory ussage <b>2 * O(NxM)</b>
      * </li>
-     * <li>Priorizado el performance por encima de la complejidad</li>
-     * <li>Asume que el adn está correctamente definido</li>
+     * <li>Performance priorized over complexity</li>
+     * <li>Preconditions: DNA should be valid, See {@link DnaValidator}</li>
      * </ul>
      * </p>
      *
@@ -36,48 +36,53 @@ public class DnaEvaluatorImpl implements DnaEvaluator {
      */
     @Override
     public boolean isHuman(@NonNull Collection<String> dna) {
-        return !isMutant(dna);
+        return !isMutant(dna.toArray(new String[0]));
     }
 
     /**
      * Look for a mutation. Prioritized performance over simplicity as required.
      *
-     * @param dna DNA Sequence
+     * @param dna DNA sequence
      * @return {@code true} when a mutation is found
      */
-    private boolean isMutant(Collection<String> dna) {
-        final int colSize = dna.stream().findFirst().map(String::length).orElseThrow();
-        final int dnaSize = dna.size();
+    private boolean isMutant(String[] dna) {
+        final int colSize = dna[0].length();
+        final int dnaSize = dna.length;
         final int[] horizontalCount = new int[dnaSize];
         final int[] verticalCount = new int[colSize];
         final int[] norWestCount = new int[colSize * dnaSize];
         final int[] norEstCount = new int[colSize * dnaSize];
-        final String[] sequences = dna.toArray(new String[dnaSize]);
 
         boolean notFirstRow = false;
         for (int row = 0; row < dnaSize; row++) { // O(N)
             boolean notFirstCol = false;
             for (int col = 0; col < colSize; col++) { // O(N)
-                int currentBase = baseAt(sequences, row, col); // O(1)
-                if (notFirstCol && baseAt(sequences, row, col - 1) == currentBase) {
+                int currentBase = baseAt(dna, row, col); // O(1)
+                if (notFirstCol && baseAt(dna, row, col - 1) == currentBase) {
                     horizontalCount[row]++;
                     if (horizontalCount[row] == MUTATION_REPETITION_COUNT) return true;
                 } else {
                     horizontalCount[row] = 1;
                 }
-                if (notFirstRow && baseAt(sequences, row - 1, col) == currentBase) {
+                if (notFirstRow && baseAt(dna, row - 1, col) == currentBase) {
                     verticalCount[col]++;
                     if (verticalCount[col] == MUTATION_REPETITION_COUNT) return true;
                 } else {
                     verticalCount[col] = 1;
                 }
-                if (notFirstCol && notFirstRow && baseAt(sequences, row - 1, col - 1) == currentBase) {
-                    norWestCount[row * colSize + col] = norWestCount[(row - 1) * colSize + (col - 1)] + 1;
-                    if (norWestCount[row * colSize + col] == MUTATION_REPETITION_COUNT - 1) return true;
+                if (notFirstCol && notFirstRow && baseAt(dna, row - 1, col - 1) == currentBase) {
+                    norWestCount[row * colSize + col] =
+                            norWestCount[(row - 1) * colSize + (col - 1)] + 1;
+                    if (norWestCount[row * colSize + col] == MUTATION_REPETITION_COUNT - 1)
+                        return true;
                 }
-                if (col < colSize - 1 && notFirstRow && baseAt(sequences, row - 1, col + 1) == currentBase) {
-                    norEstCount[row * colSize + col] = norEstCount[(row - 1) * colSize + (col + 1)] + 1;
-                    if (norEstCount[row * colSize + col] == MUTATION_REPETITION_COUNT - 1) return true;
+                if (col < colSize - 1 && notFirstRow && baseAt(dna,
+                        row - 1,
+                        col + 1) == currentBase) {
+                    norEstCount[row * colSize + col] =
+                            norEstCount[(row - 1) * colSize + (col + 1)] + 1;
+                    if (norEstCount[row * colSize + col] == MUTATION_REPETITION_COUNT - 1)
+                        return true;
                 }
                 notFirstCol = true;
             }
@@ -87,8 +92,8 @@ public class DnaEvaluatorImpl implements DnaEvaluator {
         return false;
     }
 
-    private int baseAt(String[] sequences, int row, int col) {
-        return sequences[row].codePointAt(col);
+    private int baseAt(String[] dna, int row, int col) {
+        return dna[row].codePointAt(col);
     }
 
 }
