52177c1859369a569a405455977fe094cce2e373
mrsoto <mrsoto@users.noreply.github.com>
2020-02-11 17:56:22 -0300

    agrega soporta transacciones.
    No implementé la recuperación parcial de inserts,

--- src/main/java/me/mrs/mutantes/servicios/MutantServicesApplication.java
+++ src/main/java/me/mrs/mutantes/servicios/MutantServicesApplication.java
@@ -8,11 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @EnableCaching
 @SpringBootApplication
+@EnableTransactionManagement
 public class MutantServicesApplication {
 
     public static void main(String[] args) {
